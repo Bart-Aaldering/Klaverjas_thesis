7a6c9af22d951ee484b6e79ef7cdbcf035c0050b
Bart <ba.aaldering@gmail.com>
2023-04-20 20:46:43 +0200

    disabled gpu

--- AlphaZero/value_network.py
+++ AlphaZero/value_network.py
@@ -1,19 +1,21 @@
 import numpy as np
 import tensorflow as tf
+import os
 
 from AlphaZero.state import State
 
-gpus = tf.config.list_physical_devices('GPU')
-if gpus:
-  # Restrict TensorFlow to only use the first GPU
-  try:
-    tf.config.set_visible_devices(gpus[0], 'GPU')
-    logical_gpus = tf.config.list_logical_devices('GPU')
-    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
-  except RuntimeError as e:
-    # Visible devices must be set before GPUs have been initialized
-    print(e)
+# gpus = tf.config.list_physical_devices('GPU')
+# if gpus:
+#   # Restrict TensorFlow to only use the first GPU
+#   try:
+#     tf.config.set_visible_devices(gpus[0], 'GPU')
+#     logical_gpus = tf.config.list_logical_devices('GPU')
+#     print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+#   except RuntimeError as e:
+#     # Visible devices must be set before GPUs have been initialized
+#     print(e)
 
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 class Value_network:
     def __init__(self) -> None:
