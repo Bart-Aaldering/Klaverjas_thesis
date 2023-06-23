73a237dafd11137fe39f649da06f20ead1997e57
Bart <ba.aaldering@gmail.com>
2023-06-23 15:56:06 +0200

    Revert "changed mcts output"
    This reverts commit a65c3e32cd28aedfb63d5a0b64dae95919b34bf4.

--- AlphaZero/AlphaZeroPlayer/alphazero_player.py
+++ AlphaZero/AlphaZeroPlayer/alphazero_player.py
@@ -1,6 +1,5 @@
 from __future__ import annotations  # To use the class name in the type hinting
 
-import numpy as np
 from Lennard.rounds import Round
 from AlphaZero.AlphaZeroPlayer.Klaverjas.card import Card
 from AlphaZero.AlphaZeroPlayer.Klaverjas.state import State
@@ -8,12 +7,7 @@ from AlphaZero.AlphaZeroPlayer.mcts import MCTS
 
 
 class AlphaZero_player:
-    def __init__(
-        self,
-        player_position: int,
-        mcts_params: dict,
-        model,
-    ):
+    def __init__(self, player_position: int, mcts_params: dict, model):
         self.player_position = player_position
         self.model = model
         self.mcts = MCTS(mcts_params, model, player_position)
@@ -35,14 +29,8 @@ class AlphaZero_player:
     def update_state(self, move: Card):
         self.state.do_move(move)
 
-    def get_move(self, training: bool = False, extra_noise_ratio: float = 0):
+    def get_move(self, training: bool = False):
         if self.player_position != self.state.current_player:
             print(self.state.current_player, self.player_position)
             raise Exception("Not this player's turn")
-        moves = self.mcts(self.state)
-
-        if training:
-            moves[:, 0] += int(self.mcts.mcts_steps * extra_noise_ratio)
-            return np.random.choice(moves[:, 1], p=moves[:, 0] / np.sum(moves[:, 0]))
-        else:
-            return moves[np.argmax(moves[:, 0]), 1]
+        return self.mcts(self.state, training)
