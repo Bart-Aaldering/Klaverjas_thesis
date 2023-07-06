03c25d7f552011befdcfc37fb4b00508fabca593
Bart Aaldering <s2969866@nodelogin02.cm.cluster>
2023-07-06 18:04:53 +0200

    updated testing

--- AlphaZero/AlphaZeroPlayer/mcts.py
+++ AlphaZero/AlphaZeroPlayer/mcts.py
@@ -66,14 +66,14 @@ class MCTS:
         self.tijden2 = [0, 0, 0]
 
     def __call__(self, state: State, training: bool, extra_noise_ratio):
+
+        legal_moves = state.legal_moves()
+        if len(legal_moves) == 1:
+            return next(iter(legal_moves))
+        
         current_state = copy.deepcopy(state)
         current_node = MCTS_Node()
-        if training:
-            # ucb_c = self.ucb_c * 4
-            ucb_c = self.ucb_c
-        else:
-            ucb_c = self.ucb_c
-        # current_state.set_determinization()
+
         for _ in range(self.mcts_steps):
 
             now = time.time()
@@ -86,7 +86,7 @@ class MCTS:
             while (
                 not current_state.round_complete() and current_node.legal_moves - current_node.children_moves == set()
             ):
-                current_node = current_node.select_child_ucb(ucb_c)
+                current_node = current_node.select_child_ucb(self.ucb_c)
                 current_state.do_move(current_node.move, "mcts_move")
                 current_node.set_legal_moves(current_state)
             self.tijden[1] += time.time() - now
@@ -95,8 +95,6 @@ class MCTS:
             if not current_state.round_complete():
                 new_node = current_node.expand()
                 current_node = new_node
-                # current_node.expand()
-                # current_node = current_node.select_child_ucb(ucb_c)
                 current_state.do_move(current_node.move, "mcts_move")
 
             self.tijden[2] += time.time() - now
--- run_test.py
+++ run_test.py
@@ -6,7 +6,7 @@ import math
 from AlphaZero.test_alphazero import run_test_multiprocess
 
 
-def main():
+def run_test(mcts_steps, ucb):
     try:
         n_cores = int(os.environ["SLURM_JOB_CPUS_PER_NODE"])
         cluster = "cluster"
@@ -17,16 +17,16 @@ def main():
     opponent = "rule"
     multiprocessing = True
 
-    num_rounds = 500
+    num_rounds = 5000
     num_rounds = (
         math.ceil(num_rounds / n_cores) * n_cores
     )  # make sure rounds is divisible by n_cores and not devide to 0
 
     mcts_params = {
-        "mcts_steps": 10,
+        "mcts_steps": mcts_steps,
         "n_of_sims": 1,
         "nn_scaler": 0,
-        "ucb_c": 50,
+        "ucb_c": ucb,
     }
 
     model_paths = [None, None]
@@ -43,11 +43,12 @@ def main():
         "model_paths:",
         model_paths,
     )
-
+    now = time.time()
     scores_round, alpha_eval_time, _ = run_test_multiprocess(
         n_cores, opponent, num_rounds, mcts_params, model_paths, multiprocessing
     )
-
+    print("results", mcts_params)
+    print("time:", time.time() - now)
     mean_score = sum(scores_round) / len(scores_round)
 
     print(
@@ -58,8 +59,9 @@ def main():
         "eval_time(ms):",
         alpha_eval_time,
     )
-
-
+def main():
+    run_test(200, 12800)
+    
 if __name__ == "__main__":
     start_time = time.time()
     main()
